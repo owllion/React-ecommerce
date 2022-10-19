a1bc03421348178b8289b5d721331516554c0ddb
owllion <defrag55345@gmail.com>
2022-10-19 11:13:08 +0800

    Remove dead code in SendLinkNotifixation page

--- src/components/Auth/SendLinkNotification.tsx
+++ src/components/Auth/SendLinkNotification.tsx
@@ -3,11 +3,10 @@ import { useLocation } from "react-router-dom";
 
 import CheckEmailImg from "../../assets/login/check-email.png";
 import AuthFormTemplate from "./AuthFormTemplate";
-import AuthBtn from "./AuthBtn";
 
 const SendLinkNotification = () => {
   const location = useLocation();
-  const { email, type } = location.state as { email: string; type: string };
+  const { type } = location.state as { type: string };
 
   return (
     <AuthFormTemplate
@@ -15,11 +14,7 @@ const SendLinkNotification = () => {
       subTitle={`A ${type} link was sent,   Don't forget to check your spam box.`}
       imgUrl={CheckEmailImg}
       alt="checkEmailImg"
-    >
-      {/* <Link to={"/auth/user-login"} state={{ email }}>
-        <AuthBtn btnText="Back To Login" />
-      </Link> */}
-    </AuthFormTemplate>
+    ></AuthFormTemplate>
   );
 };
 
